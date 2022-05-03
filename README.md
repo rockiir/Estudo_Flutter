487951381ecef7bc0c38c577762c3cfbb783e564
rockiir <raquelmatos09@yahoo.com.br>
2022-05-02 23:37:00 -0300

    variaveis

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Estudo_Flutter
+# Estudo Flutter
+
+Repositório destinado ao Estudo de Dart e Flutter conhecimentos adquiridos em cursos online e na faculdade.
+
+---
+
+## Índice
+
+- variaveis
+- Estudo geral
+  - Básico
+  - Exercícios de Lógica
+    - Projetos
+    - Experimentos variados
+
+---
+
+## Projetos
---
+++ flutter/teste/lib/variaveis.dart
@@ -0,0 +1,33 @@
+//variaveis
+void main() {
+  var nome;
+  nome = "Jose";
+
+  print(nome + " " + nome + " " + nome + "");
+}
+// adição
+
+void main() {
+  var salario = 100;
+
+  var bonus = 20;
+
+  var total = salario + bonus;
+
+  print(total);
+
+  print("o total é $total");
+}
+
+//tipos de variaveis
+void main() {
+  var nome = "jose pinto";
+  String email = "jamilton@gmail.com";
+  int numero = 502;
+  double preco = 19.99;
+  bool boleano = true;
+
+  var total = email;
+
+  print(nome + email);
+}
